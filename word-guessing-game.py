31fc50ddc9da0b5c65106e413ff8f8ba179f6848
CarlosEH7 <carloshueck1@gmail.com>
2025-06-08 20:58:59 +0000

    full code

--- word-guessing-game.py
+++ word-guessing-game.py
@@ -22,6 +22,37 @@ guesses = ''
 #the number of attempts the user has to guess
 turns = 12
 
+while turns > 0:
+    
+    failed = 0
+        
+        for char in word:
+            
+            if char in guesses:
+                print(char, end=" ")
+            else:
+                print("_")
+                failed += 1
+
+        if failed == 0:
+            print("You Win")
+            print("The word is : ", word)
+            break
+        
+        print()
+        
+        guess = input("guess a character:")
+            
+        guesses += guess
+                
+        if guess not in word:
+            turns -= 1
+            print("Wrong")
+            print("You have ", + turns, " more guesses")
+                
+            if turns == 0:
+                print("You Lose")
+
 
 
 
