d1e65b0960a5a798a565821221c771a30c40f2cc
CarlosEH7 <carloshueck1@gmail.com>
2025-06-08 21:03:10 +0000

    fixed indentation error

--- word-guessing-game.py
+++ word-guessing-game.py
@@ -26,32 +26,32 @@ while turns > 0:
     
     failed = 0
         
-        for char in word:
+    for char in word:
             
-            if char in guesses:
-                print(char, end=" ")
-            else:
-                print("_")
-                failed += 1
+        if char in guesses:
+            print(char, end=" ")
+        else:
+            print("_")
+            failed += 1
 
-        if failed == 0:
-            print("You Win")
-            print("The word is : ", word)
-            break
+    if failed == 0:
+        print("You Win")
+        print("The word is : ", word)
+        break
         
-        print()
+    print()
         
-        guess = input("guess a character:")
+    guess = input("guess a character:")
             
-        guesses += guess
+    guesses += guess
                 
-        if guess not in word:
-            turns -= 1
-            print("Wrong")
-            print("You have ", + turns, " more guesses")
+    if guess not in word:
+        turns -= 1
+        print("Wrong")
+        print("You have ", + turns, " more guesses")
                 
-            if turns == 0:
-                print("You Lose")
+        if turns == 0:
+            print("You Lose")
 
 
 
